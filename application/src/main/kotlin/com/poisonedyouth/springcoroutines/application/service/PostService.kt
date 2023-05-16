56d534e9a6db68d418418f8879ba80ef1a71a3ea
Matthias Schenk <matthias.schenk@sevdesk.de>
2023-05-16 10:50:57 +0200

    [V-4-5] Added timeout and cancellation for accessing external APIs

--- application/src/main/kotlin/com/poisonedyouth/springcoroutines/application/service/PostService.kt
+++ application/src/main/kotlin/com/poisonedyouth/springcoroutines/application/service/PostService.kt
@@ -6,10 +6,13 @@ import com.poisonedyouth.coroutines.domain.service.port.PostServicePort
 import com.poisonedyouth.springcoroutines.application.inbound.ExternalApi1
 import com.poisonedyouth.springcoroutines.application.inbound.ExternalApi2
 import com.poisonedyouth.springcoroutines.application.inbound.toPost
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.UUID
 import java.util.concurrent.Callable
+import java.util.concurrent.CancellationException
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 
 @Service
@@ -18,6 +21,7 @@ class PostService(
     private val externalApi1: ExternalApi1,
     private val externalApi2: ExternalApi2
 ) : PostServicePort {
+    private val logger = LoggerFactory.getLogger(PostService::class.java)
 
     private val executorService = Executors.newCachedThreadPool()
 
@@ -34,9 +38,14 @@ class PostService(
     }
 
     override fun getAllPostsAggregated(): List<Post> {
-        val callable1 = Callable { externalApi1.getPosts().map { it.toPost() } }
-        val callable2 = Callable { externalApi2.getPosts().map { it.toPost() } }
-        val result = executorService.invokeAll(listOf(callable1, callable2))
-        return result.flatMap { it.get() }
+        return try {
+            val callable1 = Callable { externalApi1.getPosts().map { it.toPost() } }
+            val callable2 = Callable { externalApi2.getPosts().map { it.toPost() } }
+            val result = executorService.invokeAll(listOf(callable1, callable2), 4, TimeUnit.SECONDS)
+            result.flatMap { it.get() }
+        } catch (e: CancellationException) {
+            logger.error("Failed to get result in time.", e)
+            emptyList()
+        }
     }
 }
